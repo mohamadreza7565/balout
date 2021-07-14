063e355645ee1277083e6959b2eba3cc01058edc
Mohamadreza Allahgholi <engineer.it93@gmail.com>
2021-07-14 11:40:20 +0430

    fix bug

--- app/src/main/java/com/rymo/balout/data/ApiData.kt
+++
@@ -1,6 +0,0 @@
-package com.rymo.ciel.data
-
-val BASE_IMAGE_URL: String = "http://engineerit93.ir/ciel/images"
-val BASE_PROFILE_IMAGE_URL : String = BASE_IMAGE_URL + "/profile/"
-val BASE_DOC_IMAGE_URL : String = BASE_IMAGE_URL + "/docs/"
-val BASE_HAIRDRESSER_IMAGE_URL : String = BASE_IMAGE_URL + "/hairdresser/"
